78c77c3560b3f222a7cad629a08614937816f809
Andy Ghiuta <andy.ghiuta@gmail.com>
2018-02-11 10:07:45 +0200

    Form elements and DOM handlers

--- course2/public/guess.js
+++ course2/public/guess.js
@@ -3,11 +3,12 @@ let guessTheWord = function(){
   // "config" options
   const MAX_TRIES = 6;
   const BONUS_PER_LETTER = 5; // bitcoins
-
   // declare an object for "word list"
   // keep a reference for the currently selected word
   // declare an object for the current state of the word
 
+  // declare a function that generates a random integer between two numbers
+  
   // initialize the guessing game
   let init = function() {
     // reset game state
@@ -15,25 +16,64 @@ let guessTheWord = function(){
     // return the number of letters in the word
   };
 
-  // will receive a letter as argument and will return if the letter exists or not in the word
+  // will receive a letter as argument and will return the updated state
   let guessLetter = function() {
+
+  };
+
+  let isValidLetter = function() {
 
   };
 
   // return an object with the functions we want exposed
   return {
     init,
-    guessLetter
+    guessLetter,
+    isValidLetter
   };
 };
 
 // when the page is initialized, init the game and
 $(document).ready(function(){
   // declare the game variable
+  let game = guessTheWord();
 
   // initialize the game
+  let initialWordState = game.init();
 
-  let word = Array(5); // replace 5 by the letter count returned from the game initialize
-  word.fill('<span class="badge badge-secondary">_</span>');
-  $('#word').html(word.join('\n'));
+  let updateWord = function(word){
+    let wordLetters = '';
+    word.forEach((letter) => {
+      wordLetters += `<span class="badge badge-secondary">${letter}</span>\n`;
+    });
+    $('#word').html(wordLetters);
+  }
+
+  // update the view with the initial word state
+  updateWord(initialWordState);
+
+  // update score function
+  let updateScore = function(newScore){
+      $('#staticScore').val(newScore);
+  };
+
+  // guess button click handler
+  $('#guess').click(function(){
+    let guessedLetter = $('#inputLetter').val();
+    // check the input of the letter
+    if (game.isValidLetter(guessedLetter)) {
+      // guess letter
+      // update the view with the new word state
+      // update the score
+    } else {
+      $('#inputLetter').popover({
+        trigger: 'manual',
+        content: 'The input is not a valid letter'
+      }).popover('show');
+      setTimeout(() => {
+        $('#inputLetter').popover('hide');
+      }, 5000);
+    }
+  });
+
 });
